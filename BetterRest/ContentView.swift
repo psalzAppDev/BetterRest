281a0e6924011275276eea1de19e18bd02538f95
Peter Salz <psalz.app.dev@web.de>
2020-06-30 13:38:44 +0200

    UI polish

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State private var wakeUp = Date()
+    @State private var wakeUp = defaultWakeTime
     @State private var sleepAmount = 8.0
     @State private var coffeeAmount = 1
     
@@ -18,9 +18,18 @@ struct ContentView: View {
     @State private var alertMessage = ""
     @State private var showingAlert = false
     
+    static var defaultWakeTime: Date {
+        
+        var components = DateComponents()
+        components.hour = 7
+        components.minute = 0
+        
+        return Calendar.current.date(from: components) ?? Date()
+    }
+    
     var body: some View {
         NavigationView {
-            VStack {
+            Form {
                 Text("When do you want to wake up?")
                     .font(.headline)
                 
@@ -30,29 +39,35 @@ struct ContentView: View {
                     displayedComponents: .hourAndMinute
                 )
                 .labelsHidden()
+                // Enforce wheel date picker style
+                //.datePickerStyle(WheelDatePickerStyle())
                 
-                Text("Desired amount of sleep")
-                    .font(.headline)
-                
-                Stepper(
-                    value: $sleepAmount,
-                    in: 4...12,
-                    step: 0.25
-                ) {
-                    Text("\(sleepAmount, specifier: "%g") hours")
+                VStack(alignment: .leading, spacing: 0) {
+                    Text("Desired amount of sleep")
+                        .font(.headline)
+                    
+                    Stepper(
+                        value: $sleepAmount,
+                        in: 4...12,
+                        step: 0.25
+                    ) {
+                        Text("\(sleepAmount, specifier: "%g") hours")
+                    }
                 }
                 
-                Text("Daily coffee intake")
-                    .font(.headline)
-                
-                Stepper(
-                    value: $coffeeAmount,
-                    in: 1...20
-                ) {
-                    if coffeeAmount == 1 {
-                        Text("1 cup")
-                    } else {
-                        Text("\(coffeeAmount) cups")
+                VStack(alignment: .leading, spacing: 0) {
+                    Text("Daily coffee intake")
+                        .font(.headline)
+                    
+                    Stepper(
+                        value: $coffeeAmount,
+                        in: 1...20
+                    ) {
+                        if coffeeAmount == 1 {
+                            Text("1 cup")
+                        } else {
+                            Text("\(coffeeAmount) cups")
+                        }
                     }
                 }
             }
